2bf9b9bf336b8673ab61b44ac9c997a4442e95fc
KimChanJin97 <102044895+KimChanJin97@users.noreply.github.com>
2022-07-30 20:21:21 +0900

    back/front 개발환경 세팅

---
+++ back/back/urls.py
@@ -0,0 +1,17 @@
+from xml.etree.ElementInclude import include
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+    import debug_toolbar
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
